2d4464dc608002680aa5e2814330c6d1b3e17079
INV8 <chaiyapat.sr@ku.th>
2026-09-02 03:54:16 +0700

    vite config log

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,20 +3,23 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/ledture/',
-  server: {
-    // Forward API calls to the Spring Boot backend during development so the
-    // frontend can use relative `/api/...` URLs without CORS issues.
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BASE_API_URL,
-        changeOrigin: true,
+export default ({ }) => {
+  console.log({...process.env})
+  return defineConfig({
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    base: '/ledture/',
+    server: {
+      // Forward API calls to the Spring Boot backend during development so the
+      // frontend can use relative `/api/...` URLs without CORS issues.
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BASE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  })
+}
