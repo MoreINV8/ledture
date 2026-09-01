7c1568e12213e6434bc95fc11afc5f3a5028167a
INV8 <chaiyapat.sr@ku.th>
2026-09-02 02:28:19 +0700

    deploy backend

--- backend/src/main/java/com/moreinv8/github/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/moreinv8/github/backend/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:4173"));
+        config.setAllowedOrigins(List.of("https://moreinv8.github.io/ledture/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
